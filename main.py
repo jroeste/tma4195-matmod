c173a5db57a5b53718dc103e17ac217f5b286879
Julie Røste <julir@stud.ntnu.no>
2019-11-10 13:50:07 +0100

    added changes to cases

--- main.py
+++ main.py
@@ -5,23 +5,37 @@ import parameters as pm
 import matplotlib.pyplot as plt
 
 
+# Total time is timeSteps * k
+timeSteps = 50000
 
-def cases(case):
-    if case == 1:
-        u = 1
-        s_matrix = ut.initialize_1(pm.spaceSteps, pm.timeSteps)
-        boundary = 'closed'
-    elif case == 2:
-        u = 0
-        s_matrix = ut.initialize_2(pm.spaceSteps, pm.timeSteps)
-        boundary = 'open'
-    return u, s_matrix, boundary
+# Total space is spaceSteps * h
+spaceSteps = 101
 
-u, s_matrix ,boundary = cases(2)
-w=np.zeros(len(s_matrix[0]))
-animationInterval = 5
-s_matrix = ut.upwind(s_matrix, pm.timeSteps, u, pm.k, pm.phi, pm.h, pm.mu_g, pm.mu_w, pm.a, w, boundary = boundary)
-pl.plotting_17(s_matrix, pm.spaceSteps, pm.timeSteps, animationInterval, pm.h, pm.k)
+case = "task7" # "task17"
+
+if case == "task7":
+    u = 0.1 # speed
+    s_matrix = ut.initialize_1(spaceSteps, timeSteps)
+    boundary = 'closed'
+    k = 0.01 # delta t
+    h = 0.1 # delta x
+    K = 1e-12 # permeability
+    dp_c = -1e6 # pressure
+
+elif case == "task17":
+    u = 0
+    s_matrix = ut.initialize_2(spaceSteps, timeSteps)
+    boundary = 'open'
+    k = 0.01 # delta t
+    h = 0.1 # delta x
+    K = 1e-8 # permeability
+    dp_c = -(pm.rho_w - pm.rho_g) * 9.81 * 3 # pressure
+
+
+w = np.zeros(len(s_matrix[0]))
+animationInterval = 10
+s_matrix = ut.upwind(s_matrix, timeSteps, u, k, pm.phi, h, pm.mu_g, pm.mu_w, K * dp_c / pm.mu_w, w, boundary = boundary)
+pl.plotting_7(s_matrix, spaceSteps, timeSteps, animationInterval, h, k) # plotting_7 or plotting_17
 
 
 
--- parameters.py
+++ parameters.py
@@ -1,11 +1,3 @@
-# Total time is timeSteps * k
-timeSteps=50000
-k=0.1
-
-# Total space is spaceSteps * h
-spaceSteps=101
-h=0.1
-
 ### Parameters ###
 
 # Porousity
@@ -19,11 +11,3 @@ mu_w=8.9e-4
 rho_g = 1.98
 rho_w = 997
 
-# Permeability
-K = 1e-9
-
-# Pressure
-#dp_c = -1e6
-dp_c = -(rho_w-rho_g)*9.81*3
-
-a = K*dp_c/mu_w

8fc074dbf90cc2535470e4b2e2e4cf3e13557ecb
Joakim Olsen <joakiol@stud.ntnu.no>
2019-11-11 23:07:26 +0100

    task 17_2 and 21

--- main.py
+++ main.py
@@ -6,36 +6,73 @@ import matplotlib.pyplot as plt
 
 
 # Total time is timeSteps * k
-timeSteps = 50000
+timeSteps = 500001
 
 # Total space is spaceSteps * h
 spaceSteps = 101
 
-case = "task7" # "task17"
+case = "task17_2" # "task17"
 
 if case == "task7":
-    u = 0.1 # speed
+    u = 2 # speed
     s_matrix = ut.initialize_1(spaceSteps, timeSteps)
     boundary = 'closed'
-    k = 0.01 # delta t
+    k = 0.001 # delta t
     h = 0.1 # delta x
-    K = 1e-12 # permeability
+    K = 1e-10 # permeability
     dp_c = -1e6 # pressure
+    plot_func_ani = pl.plotting_7
+    plot_func = pl.plot_report_7
+    name = 'task7.pdf'
 
-elif case == "task17":
+
+elif case == "task17_1":
+    u = 2
+    s_matrix = ut.initialize_1(spaceSteps, timeSteps)
+    boundary = 'closed'
+    k = 0.001 # delta t
+    h = 0.1 # delta x
+    K = 1e-10 # permeability
+    dp_c = -(pm.rho_w - pm.rho_g) * 9.81 * 100 # pressure
+    plot_func_ani = pl.plotting_17
+    plot_func = pl.plot_report_17
+    name = 'task17_1.pdf'
+
+
+elif case == "task17_2":
     u = 0
     s_matrix = ut.initialize_2(spaceSteps, timeSteps)
     boundary = 'open'
-    k = 0.01 # delta t
+    k = 0.001 # delta t
     h = 0.1 # delta x
-    K = 1e-8 # permeability
-    dp_c = -(pm.rho_w - pm.rho_g) * 9.81 * 3 # pressure
+    K = 1e-10 # permeability
+    dp_c = -(pm.rho_w - pm.rho_g) * 9.81 * 100 # pressure
+    plot_func_ani = pl.plotting_17
+    plot_func = pl.plot_report_17
+    name = 'task17_2.pdf'
 
 
-w = np.zeros(len(s_matrix[0]))
-animationInterval = 10
-s_matrix = ut.upwind(s_matrix, timeSteps, u, k, pm.phi, h, pm.mu_g, pm.mu_w, K * dp_c / pm.mu_w, w, boundary = boundary)
-pl.plotting_7(s_matrix, spaceSteps, timeSteps, animationInterval, h, k) # plotting_7 or plotting_17
 
 
+animationInterval = 1
+#s_matrix = ut.upwind(s_matrix, timeSteps, u, k, pm.phi, h, pm.mu_g, pm.mu_w, K * dp_c / pm.mu_w, boundary = boundary)
+#plot_func_ani(s_matrix, spaceSteps, timeSteps, animationInterval, h, k) # plotting_7 or plotting_17
+
+#plot_func(s_matrix, h, spaceSteps, name = name)
+
+u = 0
+s_matrix = ut.initialize_3(spaceSteps, timeSteps, h)
+boundary = 'open'
+k = 0.001 # delta t
+h = 0.1 # delta x
+K = 3e-10 # permeability
+g = 9.81
+H = 10
+theta = 1
+
+x = np.linspace(0, h * (spaceSteps - 1), spaceSteps)
+
+s_matrix = ut.upwind_21(s_matrix, timeSteps, u, k, pm.phi, h, pm.mu_g, pm.mu_w, pm.rho_w, pm.rho_g, K, g, H, theta, boundary)
+
+pl.plotting_21(s_matrix, spaceSteps, timeSteps, animationInterval, h, k, theta)
 
--- plotting.py
+++ plotting.py
@@ -59,6 +59,91 @@ def plotting_17(s_matrix, spaceSteps, timeSteps, interval, h, k):
 
     plt.show()
 
+def plotting_21(s_matrix, spaceSteps, timeSteps, interval, h, k, theta):
+
+    fig, ax = plt.subplots()
+    x = np.linspace(0, h * (spaceSteps - 1), spaceSteps)
+    ax = plt.axes(xlim=(-1.5,12),ylim=(1, 0-1/10*x[-1]*np.tan(theta)))
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    line, = ax.plot(x, s_matrix[0])
+    time_text = ax.text(0.8, 0.8, '', transform=ax.transAxes)
+
+    def init():  # give a clean slate to start
+        line.set_ydata([np.nan] * len(x))
+        time_text.set_text('')
+        return line,
+
+    def animate(i):
+        lower = -1/10*np.tan(theta)*x
+        upper = -1/10*np.tan(theta)*x + 1
+        y = s_matrix[i*100] - 1/10*np.tan(theta)*x
+        line.set_ydata(y)
+        if i % 100 == 0:
+            time_text.set_text('t = ' + str(round(i * k, 2)))
+        filling_grey = plt.fill_between(x, y, lower, facecolors='silver')
+        filling_blue = plt.fill_between(x, y, upper, facecolors='lightblue')
+        return line, filling_blue, filling_grey, time_text,
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init, interval=interval, blit=True, save_count=timeSteps/100)
+
+    plt.show()
+
+def plot_report_7(s_matrix, h, spaceSteps, name):
+    x = np.linspace(0, h * (spaceSteps - 1), spaceSteps)
+    fig, axs = plt.subplots(1, 4, figsize=(12, 4), sharey=True)
+
+    axs[0].plot(x, s_matrix[0])
+    axs[0].set_title("t = 0 s")
+    axs[0].set_xlabel('x')
+    axs[0].set_ylabel('Gas saturation s')
+    axs[1].plot(x, s_matrix[2000])
+    axs[1].set_title("t = 2 s")
+    axs[1].set_xlabel('x')
+    axs[2].plot(x, s_matrix[10000])
+    axs[2].set_title("t = 10 s")
+    axs[2].set_xlabel('x')
+    axs[3].plot(x, s_matrix[-1])
+    axs[3].set_title("t = 50 s")
+    axs[3].set_xlabel('x')
+
+    #plt.savefig(name)
+    plt.show()
+
+
+def plot_report_17(s_matrix, h, spaceSteps, name):
+    x = np.linspace(0, h * (spaceSteps - 1), spaceSteps)
+    fig, axs = plt.subplots(1, 4, figsize=(12, 4), sharey=True)
+    axs[0].set_ylim([1, 0])
+    axs[0].plot(x, s_matrix[0])
+    axs[0].fill_between(x, s_matrix[0], 0, facecolors='silver')
+    axs[0].fill_between(x, s_matrix[0], 1, facecolors='lightblue')
+    axs[0].set_title("t = 0 s")
+    axs[0].set_xlabel('x')
+    axs[0].set_ylabel('S = h/H')
+    axs[1].set_ylim([1, 0])
+    axs[1].plot(x, s_matrix[2000])
+    axs[1].fill_between(x, s_matrix[2000], 0, facecolors='silver')
+    axs[1].fill_between(x, s_matrix[2000], 1, facecolors='lightblue')
+    axs[1].set_title("t = 2 s")
+    axs[1].set_xlabel('x')
+    axs[2].set_ylim([1, 0])
+    axs[2].plot(x, s_matrix[10000])
+    axs[2].fill_between(x, s_matrix[10000], 0, facecolors='silver')
+    axs[2].fill_between(x, s_matrix[10000], 1, facecolors='lightblue')
+    axs[2].set_title("t = 10 s")
+    axs[2].set_xlabel('x')
+    axs[3].set_ylim([1, 0])
+    axs[3].plot(x, s_matrix[-1])
+    axs[3].fill_between(x, s_matrix[-1], 0, facecolors='silver')
+    axs[3].fill_between(x, s_matrix[-1], 1, facecolors='lightblue')
+    axs[3].set_title("t = 50 s")
+    axs[3].set_xlabel('x')
+
+
+    plt.savefig(name)
+    #plt.show()
 
 # def plotting_21(s_matrix, spaceSteps, timeSteps, interval, h, k):
 #
